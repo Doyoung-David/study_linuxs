179767c42211fd2070959959bc6b94966e0de9e1
Doyoung-David <brightenway@outlook.com>
2025-07-14 18:00:46 +0900

    command practice

---
+++ codes/cd_pwd_Is.sh
@@ -0,0 +1,9 @@
+# 'pwd, ls, cd' command practice
+[doyoung@localhost ~]$ cd /home/doyoung/Pictures
+[doyoung@localhost Pictures]$ pwd
+/home/doyoung/Pictures
+[doyoung@localhost Pictures]$ ls
+[doyoung@localhost Pictures]$ cd /home/doyoung/Music
+[doyoung@localhost Music]$ pwd
+/home/doyoung/Music
+[doyoung@localhost Music]$ ls
