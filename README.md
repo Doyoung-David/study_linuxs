4499c096d5b8b151a4259bc8df21535803c91e7e
Doyoung-David <brightenway@outlook.com>
2025-07-15 14:56:43 +0900

    Tree 및 Readme 변경

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# study_linuxs
-## Powershell (GUI & CLI)
-공부 내용을 올립니다.
+# Study_Linuxs
+## Powershell
+본격적인 리눅스 사용 전, Powershell을 활용한 기본기 연습
 - [Shell Command Practice](codes/10_powershells.sh)
 
--- codes/10_powershells.sh
+++ codes/10_powershells.sh
@@ -50,7 +50,7 @@ Mode                 LastWriteTime         Length Name
 d-----      2025-07-15  오전 10:22                Downloads
 d-----      2025-07-15   오전 8:33                VMs
 
-#mkdir [Making Directory Command]
+# mkdir [Making Directory Command]
 PS C:\Develops\temp_dir> mkdir dir_first
 
 
@@ -71,3 +71,84 @@ Mode                 LastWriteTime         Length Name
 ----                 -------------         ------ ----
 d-----      2025-07-15   오후 2:22                dir_first
 -a----      2025-07-15   오후 2:24              0 file_first.txt
+
+# rm [Removing Directory or File Command]
+PS C:\Develops\temp_dir> rm dir_first
+PS C:\Develops\temp_dir> ls
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----      2025-07-15   오후 2:24              0 file_first.txt
+
+
+PS C:\Develops\temp_dir> rm file_first.txt
+PS C:\Develops\temp_dir> ls
+
+# mv [Moving Files or Directories to other location]
+PS C:\Develops\temp_dir> mv file_first.txt C:\/Develops
+PS C:\Develops\temp_dir> ls
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:38                dir_first
+
+
+PS C:\Develops\temp_dir> cd C:\/Develops
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15  오전 10:22                Downloads
+d-----      2025-07-15   오후 2:46                temp_dir
+d-----      2025-07-15   오후 2:00                temp_dir_graphic
+d-----      2025-07-15   오전 8:33                VMs
+-a----      2025-07-15   오후 2:38              0 file_first.txt
+
+PS C:\Develops> cd temp_dir
+PS C:\Develops\temp_dir> mv dir_first C:\/Develops
+PS C:\Develops\temp_dir> ls
+PS C:\Develops\temp_dir> cd C:\/Develops
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:38                dir_first
+d-----      2025-07-15  오전 10:22                Downloads
+d-----      2025-07-15   오후 2:48                temp_dir
+d-----      2025-07-15   오후 2:00                temp_dir_graphic
+d-----      2025-07-15   오전 8:33                VMs
+-a----      2025-07-15   오후 2:38              0 file_first.txt
+
+
+PS C:\Develops>
+# Tree (Tree of Directories)
+PS C:\Develops\temp_dir> tree
+폴더 PATH의 목록입니다.
+볼륨 일련 번호는 52B6-33C5입니다.
+C:.
+└─dir_first
+    └─temp_dir_graphic
+PS C:\Develops\temp_dir> mv dir_first C:\/Develops
+PS C:\Develops\temp_dir> tree
+폴더 PATH의 목록입니다.
+볼륨 일련 번호는 52B6-33C5입니다.
+C:.
+
+에 하위 폴더가 없습니다.
+PS C:\Develops\temp_dir>
